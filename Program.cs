590e06319ec1a10b36281f91c191ac22f9ad14c8
Leon Voß <leon.j.voss@gmail.com>
2020-04-26 11:35:13 +0200

    Version 0.0.1 - Project Setup - Added Readme, Gitignore and csproject

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VoxelValley
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
